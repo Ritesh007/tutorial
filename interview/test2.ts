101bfd2692f77cc4a52bc736896773b5814bc70a
Ritesh Kumar Reddy Kuchukulla <rithu.ritesh@gmail.com>
2022-03-13 19:25:20 -0400

    Update test2.ts

--- interview/test2.ts
+++ interview/test2.ts
@@ -25,20 +25,45 @@ Results = ['orange', 'apple', 'pear']
 const Input_list = ["lemon", "orange", "pear", "orange", "apple", "pineapple", "watermelon", "banana", "apple", "grape", "orange", "pear"];
 const k =2;
 
-interface fruits {
-    name: string;
-    count: number;
+// interface fruits {
+//     name: string;
+//     count: number;
+// }
+
+function friutsCount(Input_list) {
+    const counts = {};
+    Input_list.forEach((x) => {
+        counts[x] = (counts[x] || 0) + 1;
+    });
+    return counts;
+}
+
+function sortObject(list) {
+    var sortable = [];
+    for (var key in list) {
+        sortable.push([key, list[key]]);
+    }
+
+    sortable.sort(function(a, b) {
+        return (a[1] > b[1] ? -1 : (a[1] < b[1] ? 1 : 0));
+    });
+
+    var orderedList = {};
+    for (var i = 0; i < sortable.length; i++) {
+        orderedList[sortable[i][0]] = sortable[i][1];
+    }
+
+    return orderedList;
 }
 
 function arrangeFruits(Input_list, k) {
-    const tempInput = [];
-    const repeatedFruits: fruits[] = [];
-    Input_list.forEach(item => {
-        if (item === tempInput) {
-            repeatedFruits.push(item, 1);
-        }
-        tempInput.push(item);
-    })
+    const friutCount = friutsCount(Input_list);
+    const friutDesecdingCount = sortObject(friutCount);
+    let arrayFruits = [];
+    for (let key in friutDesecdingCount) {
+        arrayFruits.push(key);
+    }
+    return arrayFruits.slice(0,k);
 }
 
 
